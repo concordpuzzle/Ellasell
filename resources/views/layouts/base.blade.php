f38974731b4de1eab323512772d1db8b4ceb4dda
jeremy-marple <jeremy@concordpuzzle.com>
2024-12-12 00:11:08 -0500

    h

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,13 +4,15 @@
 <head>
     @include('layouts.partials/head', ['title' => $title])
     @yield('css')
-    @vite('node_modules/aos/dist/aos.css')
-    @vite('node_modules/swiper/swiper-bundle.min.css')
-    @vite('node_modules/magnific-popup/dist/magnific-popup.css')
-    @vite('node_modules/bootstrap-select/dist/css/bootstrap-select.min.css')
-    @vite('node_modules/leaflet/dist/leaflet.css')
-    @vite('node_modules/jarallax/dist/jarallax.css')
-    @vite('resources/scss/theme.scss')
+    @vite([
+        'node_modules/aos/dist/aos.css',
+        'node_modules/swiper/swiper-bundle.min.css',
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/bootstrap-select/dist/css/bootstrap-select.min.css',
+        'node_modules/leaflet/dist/leaflet.css',
+        'node_modules/jarallax/dist/jarallax.css',
+        'resources/scss/theme.scss'
+    ])
 </head>
 
 <body>
--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,24 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // css
-                'resources/scss/theme.scss',
+                // CSS files
                 'node_modules/aos/dist/aos.css',
                 'node_modules/swiper/swiper-bundle.min.css',
                 'node_modules/magnific-popup/dist/magnific-popup.css',
                 'node_modules/bootstrap-select/dist/css/bootstrap-select.min.css',
                 'node_modules/leaflet/dist/leaflet.css',
                 'node_modules/jarallax/dist/jarallax.css',
-
-                // js
-                'resources/js/theme.js',
-                'resources/js/swiper.js'
+                'resources/scss/theme.scss',
+                
+                // Add your JS files here if needed
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources',
+        },
+    },
 });
